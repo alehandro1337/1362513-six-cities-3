26336a614e61a205ae5e0d379d515c7c746e47df
alehandro1337 <36322700+alehandro1337@users.noreply.github.com>
2020-02-11 00:45:08 +0300

    module2-task2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Main from "./components/main/main";
+import Main from "./components/main/main.jsx";
 // import App from "./components/app/app";
 
 const names = [
@@ -15,10 +15,15 @@ const names = [
 
 const offers = 3;
 
+const onMainTitleClick = () =>{
+  console.log(`Title was clicked`)
+};
+
 ReactDOM.render(
     <Main
       names={names}
       offers={offers}
+      onMainTitleClick={onMainTitleClick}
     />,
     document.getElementById(`root`)
 );
